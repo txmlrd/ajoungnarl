35bdaa245e3bea10373d50669c4706ddd3b27993
txmlrd <gungadhisanjaya@gmail.com>
2025-09-04 22:44:17 +0700

    fix: fixing sign in issue, where the button isn't centered

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,14 @@
 import { Link } from "react-router-dom";
 
-const Button = ({ text, className, path }) => {
+const Button = ({ text, className, wFull = false, path, withGoogle = false }) => {
   return (
-    <div className="flex justify-center w-full">
-      <button className={`px-[20px] py-[8px] outline-1 outline-black hover:bg-[#BCBCBC] transition-all ${className}`}>
-        <Link to={path}>{text}</Link>
-      </button>
+    <div className="w-full flex justify-center ">
+      <Link to={path || "#"} className={`justify-center items-center flex ${wFull ? "w-full" : ""}`}>
+        <button className={`flex flex-row items-center justify-center gap-2 px-[20px] py-[8px] outline-1 outline-black hover:bg-[#BCBCBC] transition-all w-full ${className}`}>
+          {withGoogle && <img src="/google.svg" alt="Google logo" className="w-[20px] h-[20px]" />}
+          <span>{text}</span>
+        </button>
+      </Link>
     </div>
   );
 };
--- src/components/Signin/Signin.jsx
+++ src/components/Signin/Signin.jsx
@@ -4,13 +4,19 @@ import Button from "../Button";
 
 const Signin = () => {
   return (
-    <div className="lg:h-[50vh] h-[80vh] flex flex-col justify-center items-center w-full">
+    <div className="lg:h-[70vh] h-[80vh] flex flex-col justify-center items-center w-full">
       <h1 className="font-cormorant font-bold lg:text-5xl text-3xl text-center">Log in or create new account</h1>
       <div className="w-full flex flex-col justify-center items-center mt-5">
-        <Form placeholder="Username" type="text" />
-        <Form placeholder="Password" type="password" />
-        <Button text={"Sign In"} className={"bg-black text-white hover:text-black lg:w-96 w-full"} />
+        <div className="flex flex-col lg:w-96 w-full justify-center items-center ">
+          <Form placeholder="Username" type="text" />
+          <Form placeholder="Password" type="password" />
+        </div>
+
+        <Button text={"Sign In"} wFull className={"bg-black text-white hover:text-black lg:w-96 w-full"} />
       </div>
+      <h1 className="my-5">or</h1>
+
+      <Button text={"Log in with Google"} wFull withGoogle className={"bg-black text-white hover:text-black lg:w-96 w-full"} />
     </div>
   );
 };
