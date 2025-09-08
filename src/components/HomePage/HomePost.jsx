7e3f8e1233d2884694d19c80001e5475a2a0c0f9
txmlrd <gungadhisanjaya@gmail.com>
2025-09-08 16:03:51 +0700

    feat: adding dynamic detailed news route, and clickable post on home

--- src/components/HomePage/HomePost.jsx
+++ src/components/HomePage/HomePost.jsx
@@ -1,5 +1,6 @@
 import { User, Clock, Calendar } from "lucide-react";
 import { usePosts } from "../../hooks/usePosts";
+import { Link } from "react-router-dom";
 
 import PostCategory from "../PostCategory";
 
@@ -13,41 +14,43 @@ const HomePost = () => {
   return (
     <div className="flex flex-col gap-10">
       {posts.map((post) => (
-        <div key={post.id} className="flex lg:flex-row flex-col gap-5">
-          <div className="rounded-sm outline-1 outline-black lg:h-[150px] h-[250px] lg:w-[200px] w-full flex-shrink-0">
-            <img src={post.image} className="w-full h-full object-cover rounded-sm" />
-          </div>
-          <div className="flex flex-col justify-between ">
-            {post.tags.map((tag) => (
-              <PostCategory key={tag} text={tag} />
-            ))}
-            <div>
-              <h1 className="font-cormorant text-[24px] font-bold">{post.title}</h1>
-              <p className="text-[14px] tracking-wide line-clamp-2">{post.content}</p>
+        <Link to={`/news/${post.id}`} key={post.id} className="no-underline text-black">
+          <div key={post.id} className="flex lg:flex-row flex-col gap-5 group">
+            <div className="rounded-sm outline-1 outline-black lg:h-[150px] h-[250px] lg:w-[200px] w-full flex-shrink-0 overflow-hidden">
+              <img src={post.image} className="w-full h-full object-cover rounded-sm group-hover:scale-105 transition-all" />
             </div>
+            <div className="flex flex-col justify-between ">
+              {post.tags.map((tag) => (
+                <PostCategory key={tag} text={tag} />
+              ))}
+              <div>
+                <h1 className="font-cormorant text-[24px] font-bold group-hover:underline">{post.title}</h1>
+                <p className="text-[14px] tracking-wide line-clamp-2">{post.content}</p>
+              </div>
 
-            <div className="flex flex-row lg:gap-4 gap-3 mt-2">
-              <div className="flex flex-row gap-1 items-center">
-                <User />
-                <p className="text-[14px]">{post.author}</p>
-              </div>
-              <div className="flex flex-row gap-1 items-center">
-                <Clock />
-                <p className="text-[14px]">{post.readTime} min read</p>
-              </div>
-              <div className="flex flex-row gap-1 items-center">
-                <Calendar />
-                <p className="text-[14px]">
-                  {post.createdAt?.toDate().toLocaleDateString("en-US", {
-                    month: "short", // Mar
-                    day: "numeric", // 5
-                    year: "numeric", // 2025
-                  })}
-                </p>
+              <div className="flex flex-row lg:gap-4 gap-3 mt-2">
+                <div className="flex flex-row gap-1 items-center">
+                  <User />
+                  <p className="text-[14px]">{post.author}</p>
+                </div>
+                <div className="flex flex-row gap-1 items-center">
+                  <Clock />
+                  <p className="text-[14px]">{post.readTime} min read</p>
+                </div>
+                <div className="flex flex-row gap-1 items-center">
+                  <Calendar />
+                  <p className="text-[14px]">
+                    {post.createdAt?.toDate().toLocaleDateString("en-US", {
+                      month: "short", // Mar
+                      day: "numeric", // 5
+                      year: "numeric", // 2025
+                    })}
+                  </p>
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        </Link>
       ))}
     </div>
   );
--- src/pages/DetailPost.jsx
+++ src/pages/DetailPost.jsx
@@ -1,12 +1,21 @@
-import React from "react";
+import { useParams } from "react-router-dom";
 import TopPost from "../components/DetailPost/TopPost";
 import Comments from "../components/DetailPost/Comments";
 import OtherNews from "../components/DetailPost/OtherNews";
+import LoadingFallback from "../helper/LoadingFallback";
+import { usePosts } from "../hooks/usePosts";
+import NotFound from "./404NotFound";
 
 const DetailPost = () => {
+  const { id } = useParams();
+  const { posts, loading } = usePosts();
+
+  if (loading) return <LoadingFallback />;
+  const post = posts.find((p) => p.id === id);
+  if (!post) return <NotFound />;
   return (
     <div className="flex flex-col gap-5 max-w-[500px] mx-auto my-10">
-      <TopPost />
+      <TopPost post={post} />
       <Comments />
       <OtherNews />
     </div>
